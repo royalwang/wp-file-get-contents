1d18bc518285d330ec84441a6cbca74e9029172e
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-22 10:26:13 +0000

    v1.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-file-get-contents/tags/1.0@936388 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-file-get-contents.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Plugin Name: wp-file-get-contents
+ * Plugin URI: http://surniaulula.com/extend/plugins/get-url/
+ * Author: Jean-Sebastien Morisset
+ * Author URI: http://surniaulula.com/
+ * License: GPLv3
+ * License URI: http://www.gnu.org/licenses/gpl.txt
+ * Description: A WordPress shortcode for PHP's file_get_contents()
+ * Requires At Least: 3.0
+ * Tested Up To: 3.9.1
+ * Version: 1.0
+ * 
+ * Copyright 2012-2014 - Jean-Sebastien Morisset - http://surniaulula.com/
+ */
+
+
+if ( ! defined( 'ABSPATH' ) ) 
+	die( 'These aren\'t the droids you\'re looking for...' );
+
+if ( ! class_exists( 'wp_file_get_contents' ) ) {
+
+	class wp_file_get_contents {
+
+		private $shortcode_tag = 'wp-file-get-contents';
+
+		public function __construct() {
+			if ( ! is_admin() ) {
+				$this->wpautop();
+				$this->add();
+			}
+		}
+
+		public function wpautop() {
+			$default_priority = 10;
+			foreach ( array( 'get_the_excerpt', 'the_excerpt', 'the_content' ) as $filter_name ) {
+				$filter_priority = has_filter( $filter_name, 'wpautop' );
+				if ( $filter_priority !== false && 
+					$filter_priority > $default_priority ) {
+
+					remove_filter( $filter_name, 'wpautop' );
+					add_filter( $filter_name, 'wpautop' , $default_priority );
+				}
+			}
+		}
+
+		public function add() {
+        		add_shortcode( $this->shortcode_tag, array( &$this, 'shortcode' ) );
+		}
+
+		public function remove() {
+			remove_shortcode( $this->shortcode_tag );
+		}
+
+		public function shortcode( $atts, $content = null ) { 
+			$cache = array_key_exists( 'cache', $atts ) ? $atts['cache'] : 300;
+
+			if ( ! empty( $atts['url'] ) && preg_match( '/^https?:\/\//', $atts['url'] ) )
+				$url = $atts['url'];
+			elseif ( ! empty( $atts['url'] ) && preg_match( '/^file:\/\//', $atts['url'] ) )
+				$url = trailingslashit( WP_CONTENT_DIR ).preg_replace( '/(^file:\/\/|\.\.)/', '', $atts['url'] );
+			elseif ( ! empty( $atts['file'] ) )
+				$url = trailingslashit( WP_CONTENT_DIR ).preg_replace( '/(^\/+|\.\.)/', '', $atts['file'] );
+			else $content = '<p>'.__CLASS__.': <em><code>url</code> or <code>file</code> shortcode attribute missing</em></p>';
+
+			if ( ! empty( $url ) ) {
+				$cache_salt = __METHOD__.'(url:'.$url.')';
+				$cache_id = __CLASS__.'_'.md5( $cache_salt );
+				if ( $cache > 0 ) {
+					$content = get_transient( $cache_id );
+					if ( $content === false ) {
+						$content = file_get_contents( $url );
+						set_transient( $cache_id, $content, $cache );
+					}
+				} else {
+					delete_transient( $cache_id );
+					$content = file_get_contents( $url );
+				}
+			}
+			return $content;
+		}
+	}
+
+        global $wp_file_get_contents;
+        $wp_file_get_contents = new wp_file_get_contents();
+}
+
+?>
